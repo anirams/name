8abceff591330013cbe91c76d65ac80b061e87e2
anirams <marina.stvoric1@gmail.com>
2019-01-28 01:12:35 +0100

    first

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article 1',
+			'body': 'A many-to-one is similar to a one-to-many relationship. The difference is that this relationship is looked at from the "many" side.',
+			'author': 'Marina',
+			'create_date': '04-19-2023'
+		},
+		{
+			'id': 2,
+			'title': 'Article 2',
+			'body': 'A many-to-one is similar to a one-to-many relationship. The difference is that this relationship is looked at from the "many" side.',
+			'author': 'Marina',
+			'create_date': '04-19-2023'
+		},
+		{
+			'id': 3,
+			'title': 'Article 3',
+			'body': 'A many-to-one is similar to a one-to-many relationship. The difference is that this relationship is looked at from the "many" side.',
+			'author': 'Marina',
+			'create_date': '04-19-2023'
+		}
+	]
+	return articles
